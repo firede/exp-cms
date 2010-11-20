26423e3596439192acf3792554a781e3d3a1d229
Zhao Lei <aicoylei@gmail.com>
2010-11-20 20:40:40 +0800

    init env script (bat/win, sh/linux)

---
+++ env-init.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+mkdir application\classes\controller
+mkdir application\classes\model
+mkdir application\views
+mkdir application\config
+mkdir application\cache
+mkdir application\logs
+
---
+++ env-init.sh
@@ -0,0 +1,4 @@
+mkdir -p application/classes/{controller,model}
+mkdir -p application/{config,views}
+mkdir -m 0777 -p application/{cache,logs}
+
