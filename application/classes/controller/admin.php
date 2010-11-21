1cd97c23a14970c38c8dec14b221f8fd0b71eedc
Zhao Lei <aicoylei@gmail.com>
2010-11-21 17:12:28 +0800

    add pagination module

--- application/classes/controller/admin.php
+++ application/classes/controller/admin.php
@@ -2,22 +2,25 @@
 
 class Controller_Admin extends Controller_Template {
 
-	public $template = 'admin/index';
+	public $template = 'admin/base/layout';
 	
 	public function action_index()
 	{
-		// 用来测试admin views的代码
-		$data['assets_admin'] = '/assets/admin/';
-		$data['assets_common'] = '/assets/common/';
-		$data['title'] = '大犀牛体验版CMS - 后台管理';
-		
-		$data['content'] = View::factory('welcome/index', array(
-			'hello'=>'Hello, World!',
+		$pagination = new Pagination(array(
+			'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
+			'total_items'       => 1234,
+			'items_per_page'    => 20,
+			'view'              => 'pagination/floating',
+			'auto_hide'         => TRUE,
+			'first_page_in_url' => FALSE,
 		));
 
-		foreach ($data as $key => $value) {
-			$this->template->$key = $value;
-		}
+		// 用来测试admin views的代码
+		$this->template->title = '大犀牛体验版CMS - 后台管理';
+
+		$this->template->layout_main = View::factory('admin/post/list', array(
+			'pagination' => $pagination,
+		));
 	}
 
 }
