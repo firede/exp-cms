f0fde9e38dff53a047364ff94cfdc5714eb11938
Zhao Lei <aicoylei@gmail.com>
2010-11-23 21:05:54 +0800

    add js and views

---
+++ assets/admin/js/admin.js
@@ -0,0 +1,18 @@
+//jQuery.fn.extend = function(){
+//
+//}
+//
+//$('<div id="js-screen-mask">')
+//	.css({
+//		position: 'absolute',
+//		top: $(document).scrollTop(),
+//		left: 0,
+//		height: $(document).height(),
+//		width: '100%',
+//
+//		opacity: 0.6,
+//		backgroundColor: 'black',
+//		zIndex: 5000
+//	})
+//	.appendTo(document.body)
+//	.hide();
