8a457d75a47da20a2a0201d24b39cf8f36fb3da7
Zhao Lei <aicoylei@gmail.com>
2010-11-24 20:25:47 +0800

    错误提示增加"返回上一页"

--- modules/helper/classes/message.php
+++ modules/helper/classes/message.php
@@ -1,9 +1,12 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
 class Message {
 	public static function admin_render($view_data) {
 		if ($view_data['success'] === FALSE) {
-			echo '<div class="message-error radius_all">'.$view_data['message'].'</div>';
+			echo '<div class="message-error radius_all">'
+					.$view_data['message']
+					.' <a href="javascript:window.history.go(-1);">返回上一页</a>'
+				.'</div>';
 			exit;
 		} elseif ($view_data['success'] === TRUE) {
 			echo ($view_data['success'] === '')
