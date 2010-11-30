c8b2dd81e552d80edb22ed4995f4bb3daf0e0412
Zhao Lei <aicoylei@gmail.com>
2010-11-30 21:31:11 +0800

    表格控件增加宽度配置,更新admin/post配置

--- application/config/admin_post.php
+++ application/config/admin_post.php
@@ -13,12 +13,14 @@ return array(
 		'column' => array(
 			'select'	    => array(
 				'label'     => '选择',
+				'width'     => '30',
 				'template'  => 'checkbox',
 				'data'      => 'id',
 				'sortable'  => FALSE,
 			),
 			'title'		    => array(
 				'label'     => '标题',
+				'width'     => '340',
 				'template'  => 'link',
 				'prefix'    => array('admin/post/view/', '.html'),
 				'data'      => 'id,title',
@@ -26,18 +28,21 @@ return array(
 			),
 			'time'          => array(
 				'label'     => '日期',
+				'width'     => '100',
 				'template'  => 'date',
 				'data'      => 'pub_time',
 				'sortable'  => TRUE,
 			),
 			'author'        => array(
 				'label'     => '作者',
+				'width'     => '100',
 				'template'  => 'link',
 				'data'      => 'user_id,user_name',
 				'sortable'  => TRUE,
 			),
 			'category'      => array(
 				'label'     => '分类',
+				'width'     => '100',
 				'template'  => 'link',
 				'data'      => 'cate_id,cate_name',
 				'sortable'  => TRUE,
@@ -52,6 +57,8 @@ return array(
 		),
 		// 批量操作（批量操作允许的功能）
 		'muti_operation' => array(
+			'select'    => '全选/取消',
+			'inverse'   => '反选',
 			'audit'     => '审核',
 			'move'      => '移动',
 			'delete'    => '删除',
@@ -61,13 +68,56 @@ return array(
 	// 修改待审核
 	'status_2' => array(
 		'column' => array(
-			'select'    => '选择',
-			'title'     => '标题',
-			'flag'      => '标记',
-			'click'     => '点击量',
-			'time'      => '日期',
-			'author'    => '作者',
-			'category'  => '分类',
+			'select'	    => array(
+				'label'     => '选择',
+				'width'     => '30',
+				'template'  => 'checkbox',
+				'data'      => 'id',
+				'sortable'  => FALSE,
+			),
+			'title'		    => array(
+				'label'     => '标题',
+				'width'     => '270',
+				'template'  => 'link',
+				'prefix'    => array('admin/post/view/', '.html'),
+				'data'      => 'id,title',
+				'sortable'  => TRUE,
+			),
+			'flag'		    => array(
+				'label'     => '',             // 标记（在表头不显示标题）
+				'width'     => '60',
+				'template'  => 'flag',
+				'data'      => 'flag',
+				'sortable'  => TRUE,
+			),
+			'click'         => array(
+				'label'     => '点击',
+				'width'     => '38',
+				'template'  => 'text',
+				'data'      => 'read_count',
+				'sortable'  => TRUE,
+			),
+			'time'          => array(
+				'label'     => '日期',
+				'width'     => '75',
+				'template'  => 'date',
+				'data'      => 'pub_time',
+				'sortable'  => TRUE,
+			),
+			'author'        => array(
+				'label'     => '作者',
+				'width'     => '90',
+				'template'  => 'link',
+				'data'      => 'user_id,user_name',
+				'sortable'  => TRUE,
+			),
+			'category'      => array(
+				'label'     => '分类',
+				'width'     => '90',
+				'template'  => 'link',
+				'data'      => 'cate_id,cate_name',
+				'sortable'  => TRUE,
+			),
 		),
 		'operation'=> array(
 			'audit'     => '快速审核',
@@ -77,6 +127,8 @@ return array(
 			'preview-ol'=> '线上版本预览',
 		),
 		'muti_operation' => array(
+			'select'    => '全选/取消',
+			'inverse'   => '反选',
 			'audit'     => '审核',
 			'move'      => '移动',
 			'delete'    => '删除',
@@ -88,43 +140,50 @@ return array(
 		'column' => array(
 			'select'	    => array(
 				'label'     => '选择',
+				'width'     => '30',
 				'template'  => 'checkbox',
 				'data'      => 'id',
 				'sortable'  => FALSE,
 			),
 			'title'		    => array(
 				'label'     => '标题',
+				'width'     => '270',
 				'template'  => 'link',
 				'prefix'    => array('admin/post/view/', '.html'),
 				'data'      => 'id,title',
 				'sortable'  => TRUE,
 			),
 			'flag'		    => array(
-				'label'     => '标记',
+				'label'     => '',             // 标记（在表头不显示标题）
+				'width'     => '60',
 				'template'  => 'flag',
 				'data'      => 'flag',
 				'sortable'  => TRUE,
 			),
 			'click'         => array(
-				'label'     => '点击量',
+				'label'     => '点击',
+				'width'     => '38',
 				'template'  => 'text',
 				'data'      => 'read_count',
 				'sortable'  => TRUE,
 			),
 			'time'          => array(
 				'label'     => '日期',
+				'width'     => '75',
 				'template'  => 'date',
 				'data'      => 'pub_time',
 				'sortable'  => TRUE,
 			),
 			'author'        => array(
 				'label'     => '作者',
+				'width'     => '80',
 				'template'  => 'link',
 				'data'      => 'user_id,user_name',
 				'sortable'  => TRUE,
 			),
 			'category'      => array(
 				'label'     => '分类',
+				'width'     => '80',
 				'template'  => 'link',
 				'data'      => 'cate_id,cate_name',
 				'sortable'  => TRUE,
@@ -150,40 +209,105 @@ return array(
 	// 草稿
 	'status_5' => array(
 		'column' => array(
-			'select'    => '选择',
-			'title'     => '标题',
-			'time'      => '日期',
-			'author'    => '作者',
-			'category'  => '分类',
+			'title'		    => array(
+				'label'     => '标题',
+				'width'     => '400',
+				'template'  => 'link',
+				'prefix'    => array('admin/post/view/', '.html'),
+				'data'      => 'id,title',
+				'sortable'  => TRUE,
+			),
+			'time'          => array(
+				'label'     => '日期',
+				'width'     => '90',
+				'template'  => 'date',
+				'data'      => 'pub_time',
+				'sortable'  => TRUE,
+			),
+			'author'        => array(
+				'label'     => '作者',
+				'width'     => '120',
+				'template'  => 'link',
+				'data'      => 'user_id,user_name',
+				'sortable'  => TRUE,
+			),
+			'category'      => array(
+				'label'     => '分类',
+				'width'     => '120',
+				'template'  => 'link',
+				'data'      => 'cate_id,cate_name',
+				'sortable'  => TRUE,
+			)
 		),
 		'operation'=> array(
 			'preview'   => '预览',
-		),
-		'muti_operation' => array(
 		),
 	),
 	
 	// 驳回
 	'status_3' => array(
 		'column' => array(
-			'select'    => '选择',
-			'title'     => '标题',
-			'flag'      => '标记',
-			'time'      => '日期',
-			'author'    => '作者',
-			'category'  => '分类',
-			'rej-type'  => '驳回类别',
+			'select'	    => array(
+				'label'     => '选择',
+				'width'     => '30',
+				'template'  => 'checkbox',
+				'data'      => 'id',
+				'sortable'  => FALSE,
+			),
+			'title'		    => array(
+				'label'     => '标题',
+				'width'     => '290',
+				'template'  => 'link',
+				'prefix'    => array('admin/post/view/', '.html'),
+				'data'      => 'id,title',
+				'sortable'  => TRUE,
+			),
+			'flag'		    => array(
+				'label'     => '',             // 标记（在表头不显示标题）
+				'width'     => '60',
+				'template'  => 'flag',
+				'data'      => 'flag',
+				'sortable'  => TRUE,
+			),
+			'time'          => array(
+				'label'     => '日期',
+				'width'     => '75',
+				'template'  => 'date',
+				'data'      => 'pub_time',
+				'sortable'  => TRUE,
+			),
+			'author'        => array(
+				'label'     => '作者',
+				'width'     => '90',
+				'template'  => 'link',
+				'data'      => 'user_id,user_name',
+				'sortable'  => TRUE,
+			),
+			'category'      => array(
+				'label'     => '分类',
+				'width'     => '90',
+				'template'  => 'link',
+				'data'      => 'cate_id,cate_name',
+				'sortable'  => TRUE,
+			),
+			'rej-type'      => array(
+				'label'     => '驳回类别',
+				'width'     => '60',
+				'template'  => 'rej_type',
+				'data'      => 'content',
+				'sortable'  => TRUE,
+			),
 		),
 		'operation'=> array(
 			'undo-rej'  => '撤销驳回',
 			'preview'   => '预览',
 		),
 		'muti_operation' => array(
+			'select'    => '全选/取消',
+			'inverse'   => '反选',
 			'undo-rej'  => '撤销驳回',
 		),
 	),
 );
 
 ?>
-
-
--- application/views/system/admintable.tpl
+++ application/views/system/admintable.tpl
@@ -6,35 +6,37 @@
 	<{/foreach}>
 </div>
 <{/if}>
-<table class="list-table">
-	<thead>
-		<tr>
-			<{foreach from=$_admintable_conf.column item=_admintable_column key=_admintable_key}>
-			<th col_id="<{$_admintable_key}>"><{$_admintable_column.label}></th>
-			<{/foreach}>
-			<{* 判断是否添加操作列表头 *}>
-			<{if $_admintable_conf.operation|default}>
-			<th>操作</th>
-			<{/if}>
-		</tr>
-	</thead>
-	<tbody>
-		<{foreach from=$_admintable_data item=_admintable_item key=_admintable_key}>
-		<tr row_id="<{$_admintable_key}>">
-			<{foreach from=$_admintable_conf.column item=_admintable_column}>
-			<td><{basetpl data=$_admintable_item conf=$_admintable_column.data tpl=$_admintable_column.template prefix=$_admintable_column.prefix|default}></td>
-			<{/foreach}>
-			<{* 判断是否添加操作列 *}>
-			<{if $_admintable_conf.operation|default}>
-			<td>
-				<{foreach from=$_admintable_conf.operation item=_admintable_item key=_admintable_key}>
-				<span class="table-btn icon-<{$_admintable_key}> js-opt-<{$_admintable_key}>" title="<{$_admintable_item}>">
-					<span><{$_admintable_item}></span>
-				</span>
+<div class="list-table-wrap">
+	<table class="list-table">
+		<thead>
+			<tr>
+				<{foreach from=$_admintable_conf.column item=_admintable_column key=_admintable_key}>
+				<th col_id="<{$_admintable_key}>"<{if $_admintable_column.width|default}> style="width:<{$_admintable_column.width}>px;"<{/if}>><{$_admintable_column.label}></th>
 				<{/foreach}>
-			</td>
-			<{/if}>
-		</tr>
-		<{/foreach}>
-	</tbody>
-</table>
+				<{* 判断是否添加操作列表头 *}>
+				<{if $_admintable_conf.operation|default}>
+				<th>操作</th>
+				<{/if}>
+			</tr>
+		</thead>
+		<tbody>
+			<{foreach from=$_admintable_data item=_admintable_item key=_admintable_key}>
+			<tr row_id="<{$_admintable_key}>">
+				<{foreach from=$_admintable_conf.column item=_admintable_column}>
+				<td><{basetpl data=$_admintable_item conf=$_admintable_column.data tpl=$_admintable_column.template prefix=$_admintable_column.prefix|default}></td>
+				<{/foreach}>
+				<{* 判断是否添加操作列 *}>
+				<{if $_admintable_conf.operation|default}>
+				<td>
+					<{foreach from=$_admintable_conf.operation item=_admintable_item key=_admintable_key}>
+					<span class="table-btn icon-<{$_admintable_key}> js-opt-<{$_admintable_key}>" title="<{$_admintable_item}>">
+						<span><{$_admintable_item}></span>
+					</span>
+					<{/foreach}>
+				</td>
+				<{/if}>
+			</tr>
+			<{/foreach}>
+		</tbody>
+	</table>
+</div>
