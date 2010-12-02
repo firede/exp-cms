d0f203ffc583c7f60647f9fc1d41b92a226d5ad0
Zhao Lei <aicoylei@gmail.com>
2010-12-02 15:06:14 +0800

    增加全选、反选样式

--- assets/admin/js/table.js
+++ assets/admin/js/table.js
@@ -1,5 +1,7 @@
 (function( $ ){
 
+	var optHoverClass = 'operation-btn-hover';
+
 	// 初始化表格
 	$('.list-table tbody tr:odd').addClass('odd');
 	$('#status-' + PAGEENV.param.status).addClass('status-tab-active');
@@ -9,13 +11,29 @@
 		$('.list-table input[name=select]').each(function() {
 			this.checked = true;
 		});
-	});
+	})
+	.hover(
+		function() {
+			$(this).addClass(optHoverClass);
+		},
+		function() {
+			$(this).removeClass(optHoverClass);
+		}
+	);
 
 	// 反选
 	$('.js-mutiopt-inverse').click(function(){
 		$('.list-table input[name=select]').each(function() {
 			this.checked = !this.checked;
 		});
-	});
+	})
+	.hover(
+		function() {
+			$(this).addClass(optHoverClass);
+		},
+		function() {
+			$(this).removeClass(optHoverClass);
+		}
+	);
 
 })( jQuery );
