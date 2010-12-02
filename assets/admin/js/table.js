8432ac3138b24fd0592f5857690792eb2bcd2bf8
Zhao Lei <aicoylei@gmail.com>
2010-12-02 15:00:31 +0800

    增加“全选”、“反选”功能

--- assets/admin/js/table.js
+++ assets/admin/js/table.js
@@ -4,4 +4,18 @@
 	$('.list-table tbody tr:odd').addClass('odd');
 	$('#status-' + PAGEENV.param.status).addClass('status-tab-active');
 
+	// 全选
+	$('.js-mutiopt-select').click(function(){
+		$('.list-table input[name=select]').each(function() {
+			this.checked = true;
+		});
+	});
+
+	// 反选
+	$('.js-mutiopt-inverse').click(function(){
+		$('.list-table input[name=select]').each(function() {
+			this.checked = !this.checked;
+		});
+	});
+
 })( jQuery );
