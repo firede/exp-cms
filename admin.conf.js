7b2eeb66fc836122892db7c9eec12e869dc8a073
Zhao Lei <aicoylei@gmail.com>
2010-12-04 16:29:16 +0800

    添加JS检查、合并、压缩配置文件与压缩后的JS

---
+++ admin.conf.js
@@ -0,0 +1,10 @@
+{
+	src					: "./assets/admin/js/src",
+	dest				: "./assets/admin/js",
+	aggregateTo			: "admin.min.js",
+	runJslint			: true,
+	runGCompiler		: true,
+	keepCompiled		: false,
+	compiledExtension	: "min",
+	order				: "util.js",
+}
