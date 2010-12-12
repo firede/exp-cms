691bdc9330402577745484f95f401c4a318ee4cd
Zhao Lei <aicoylei@gmail.com>
2010-12-12 16:15:00 +0800

    修改分类列表配置级操作图标

--- application/config/admin_category_list.php
+++ application/config/admin_category_list.php
@@ -1,9 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
- * 后台Post部分管理权限配置
- *
- * @TODO 删除权限以后作成可在后台启用/关闭的
+ * 后台分类部分权限配置
  */
 return array(
 	// 主键，用于行级操作的数据来源
@@ -22,26 +20,26 @@ return array(
 			'template'  => 'link',
 			'prefix'    => array('admin/post/list?category='),
 			'data'      => 'id,name',
-			'order_by'  => 'name',      // 配置此字段则本列启用排序
+			'order_by'  => 'name',
 		),
 		'time'          => array(
 			'label'     => '短名称',
-			'width'     => '100',
+			'width'     => '130',
 			'template'  => 'text',
 			'data'      => 'short_name',
 			'order_by'  => 'short_name',
 		),
 		'author'        => array(
 			'label'     => '父级分类',
-			'width'     => '100',
+			'width'     => '130',
 			'template'  => 'link',
 			'prefix'    => array('admin/category/list?category='),
 			'data'      => 'parent_id,parent_name',
-			'order_by'  => 'parent_id,parent_name',
+			'order_by'  => 'parent_name',
 		),
 		'category'      => array(
 			'label'     => '级别',
-			'width'     => '100',
+			'width'     => '80',
 			'template'  => 'text',
 			'data'      => 'sort',
 			'order_by'  => 'sort',
@@ -49,11 +47,11 @@ return array(
 	),
 	// 操作（行级操作允许的功能）
 	'operation'=> array(
-		'modify'    => array(
+		'cate-modify'    => array(
 			'title'     => '修改',
 			'action'    => 'admin/category/modify',
 		),
-		'del'       => array(
+		'cate-del'       => array(
 			'title'     => '删除',
 			'action'    => 'admin/category/del',
 		),
@@ -62,7 +60,7 @@ return array(
 	'muti_operation' => array(
 		'select'    => array('title' => '全选'),
 		'inverse'   => array('title' => '反选'),
-		'del'       => array(
+		'cate-del'       => array(
 			'title'     => '删除',
 			'action'    => 'admin/category/m_del',
 		),
