1c0192dc6747e32914cb84536eb3ded061bad3e0
fanqie <dc2002007@gmail.com>
2010-12-14 15:43:08 +0800

    完善了异常处理

--- modules/helper/classes/errorexceptionreport.php
+++ modules/helper/classes/errorexceptionreport.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('SYSPATH') or die('No direct script access.');
 /*
  * To change this template, choose Tools | Templates
@@ -11,26 +12,27 @@ defined('SYSPATH') or die('No direct script access.');
  * @author Administrator
  */
 class ErrorExceptionReport {
-    /******
-     *
+
+    /**     * ***
+     * 异常报告处理 在web上显示 不显示 直接抛出 写入log文件
+     * @param $e 已经捕捉成功的异常对象
      */
-     public static  function  _errors_report($e){
-       
+    public static function _errors_report($e) {
+        $message = "错误信息=>" . date("Y-m-d H:i:s") . ":" . $e->getMessage() . "\r\n" .
+                "所在文件:" . $e->getFile() . "\r\n" .
+                "所在行数:" . $e->getLine() . "\r\n" .
+                "出错的代码:" . $e->getCode() . "\r\n" .
+                "流程:" . $e->getTraceAsString() . "\r\n";
         $application = Kohana::config("applicationconfig");
         if (!$application["advanced"]["throw_exception"] == NULL) {
             if ($application["advanced"]["throw_exception"] == "FILE") {
-                $message = $e->getMessage();
-                File::create_or_add(APPPATH."/exception.log","错误信息=>".date("Y-m-d H:i:s").":".$message."\n");
+                File::create_or_add(APPPATH . "/log/" . date("Y/m/d") . "/exception_report.log", $message);
             } elseif ($application["advanced"]["throw_exception"] == "WEB") {
-               
-                $message = $e->getMessage();
-                echo "错误信息=>",date("Y-m-d H:i:s"),":".$message;
+                echo $message;
             } elseif ($application["advanced"]["throw_exception"] == "THROW") {
                 throw $e;
             }
         }
-
-        
     }
 
 }
