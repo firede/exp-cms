2e775ce9a26a40d629dd3b9dce83b613fff471c8
Zhao Lei <aicoylei@gmail.com>
2010-12-31 18:19:01 +0800

    更新图片和导航目录

--- assets/admin/js/src/asideMenu.js
+++ assets/admin/js/src/asideMenu.js
@@ -1,28 +1,27 @@
 dxn.asideMenu = (function ($) {
 	var container = $('.mainmenu'),
 		menuBtns = container.find('.exbtn'),
-		classBtnHover = 'title-hover';
+		classTitleHover = 'title-hover',
+		classBtnHover = 'exbtn-hover';
 
-	menuBtns.each(function () {
+	menuBtns.each(function (i) {
 		var el = $(this),
 			list = el.next('.sublist'),
 			title = el.prev('.title');
 
 		el.hover(
 			function () {
-				title.addClass(classBtnHover);
+				title.addClass(classTitleHover);
+				el.addClass(classBtnHover);
 			},
 			function () {
-				title.removeClass(classBtnHover);
+				title.removeClass(classTitleHover);
+				el.removeClass(classBtnHover);
 			}
 		);
 
 		el.click(function () {
-			if (list.css('display') != 'block') {
-				list.css('display', 'block');
-			} else {
-				list.css('display', 'none');
-			}
+			list.slideToggle('fast');
 			return false;
 		});
 	});
