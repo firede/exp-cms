0999bd92980ad24f5b4279797d94cc407123a147
Zhao Lei <aicoylei@gmail.com>
2011-01-15 22:33:32 +0800

    解决合并冲突

--- application/classes/controller/post.php
+++ application/classes/controller/post.php
@@ -3,12 +3,6 @@
 defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Post extends Controller_BaseUser {
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> 4260af4220263d1e0edf5abb75634fa00db4b54b
-
     /**
      * 获取 带分页的文章的列表
      * @param <type> $dxn_get
@@ -63,13 +57,9 @@ class Controller_Post extends Controller_BaseUser {
                     'pagination' => $pagination,
                     'view_data' => $posts,
                     'conf' => $conf,
-<<<<<<< HEAD
-                        ), TRUE);
-=======
                 ), TRUE);
->>>>>>> 4260af4220263d1e0edf5abb75634fa00db4b54b
 
-        echo AppCache::app_cache("post_view", AppCache::app_cache('before_post_list', $view));
+		echo AppCache::app_cache("post_view", AppCache::app_cache('before_post_list', $view));
     }
 
     /**
